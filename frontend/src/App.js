e24bcb45d9fb728592200aa3a6f2383386f7b460
João Pedro <jps12carvalho@gmail.com>
2020-11-23 16:24:55 -0300

    Adicionando pagina para atualizar os dados do usuario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ProfileUser from './views/ProfileUser';
 import HomePage from './views/HomePage';
+import { Button, Icon } from 'react-native-elements';
+import UpdateUser from './views/UpdateUser';
 
 const Stack = createStackNavigator();
 
@@ -20,18 +22,32 @@ export default (props) => {
                     component={HomePage}
                     options={title.homePage}
                 />
+                <Stack.Screen
+                    name="UpdateUser"
+                    component={UpdateUser}
+                    options={title.updateUser}
+                />
             </Stack.Navigator>
         </NavigationContainer>
     );
 };
 
 const title = {
-    profileUser: {
-        title: 'Perfil de usuário',
-        headerTitleStyle: {
+    profileUser: ({ navigation }) => {
+        return {
+            title: 'Perfil de usuário',
+            headerTitleStyle: {
             marginLeft: 15,
             fontSize: 25,
-        },
+            },
+            headerRight: () => (
+                <Button
+                    onPress={() => navigation.navigate('UpdateUser')}
+                    type='clear'
+                    icon={<Icon name='edit' color='#000000'/> }
+                />
+            ),
+        }
     },
     homePage: {
         title: 'WeServe',
@@ -39,5 +55,21 @@ const title = {
             alignSelf: 'center',
             fontSize: 25,
         },
+    },
+    updateUser: ({ navigation }) => {
+        return {
+            title: 'Atualizar cadastro',
+            headerTitleStyle: {
+                marginLeft: 15,
+                fontSize: 25,
+            },
+            headerLeft: () => (
+                <Button
+                    onPress={() => navigation.goBack()}
+                    type='clear'
+                    icon={<Icon name='close' color='#000000'/> }
+                />
+            ),
+        }
     },
 };
---
+++ frontend/src/views/UpdateUser.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import {Text} from 'react-native'
+
+
+export default class UpdateUser extends Component{
+    render(){
+        return (
+            <Text>Ola mundo</Text>
+        )
+    }
+}
