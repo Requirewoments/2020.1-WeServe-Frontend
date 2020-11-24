b922589bca225960317c05dbc10585558530eaf9
Maicon Mares <maiconmaresunb@gmail.com>
2020-11-24 16:04:41 -0300

    Adicionando validacao ao email e senha.

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -3,19 +3,34 @@ const mongoose = require('mongoose');
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        require: true,
+        required: [true, 'Name required!'],
     },
     username: {
         type: String,
-        require: false,
+        required: false,
     },
     email: {
         type: String,
-        require: true,
+        require: [true, 'Email required!'],
+        unique: true,
+        validate: {
+            validator: function(email) {
+                return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
+            },
+            message: props => `${props.value} não é um email válido!`
+        }
     },
     password: {
         type: String,
-        require: true,
+        required: [true, 'A senha é obrigatória!'],
+        unique: true,
+        validate: {
+            validator: function (password) {
+                return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/.test(password)
+            },
+            message: props => `${props.value} Sua senha deve conter 6-20 caracteres, 
+                entre eles no mínimo um número, uma letra maiúscula e uma minúscula.`
+        }
     },
     createdAt: {
         type: Date,
