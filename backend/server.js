5eaa4d053ee5740d917161b59ba72c000e9e682f
Maicon Mares <maiconmaresunb@gmail.com>
2020-11-24 14:46:23 -0300

    Resolvendo erro ao deletar User.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const {MongoClient} = require('mongodb');
+const routes = require('./routes');
 
 const app = express();
 
@@ -16,25 +16,11 @@ app.use(routes);
 
 const main = async () => {
     const uri = 'mongodb%2Bsrv%3A%2F%2Fmaiconmares%3Aqnpjeposf4580%40cluster0.pr5dc.mongodb.net%2FWeServe%3FretryWrites%3Dtrue%26w%3Dmajority';
-    const client = new MongoClient(decodeURIComponent(uri), {useNewUrlParser: true, useUnifiedTopology: true});
-
-    try {
-        await client.connect();
-
-        await listDatabases(client);
-    } catch (e) {
-        console.log(e);
-    }
+    const client = mongoose.connect(decodeURIComponent(uri), {useNewUrlParser: true, useUnifiedTopology: true});
 }
 
-const listDatabases = async client => {
-    let databasesList = await client.db().admin().listDatabases();
 
-    console.log('All is working! Databases:');
-    databasesList.databases.forEach(db => console.log(`- ${db.name}`));
-}
-
-main().catch(console.error);
+main().then().catch(console.error);
 
 /*mongoose.connect('mongodb+srv://maiconmares:qnpjeposf4580@cluster0.pr5dc.mongodb.net/test', {
     useNewUrlParser: true,
--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -1,9 +1,14 @@
 const requireDir = require('require-dir');
-requireDir('./../models');
-
+const {getClient} = require('./../utils/getClient');
 const mongoose = require('mongoose');
-
+requireDir('./../models');
 const User = mongoose.model('User');
+let client, db;
+
+getClient().then(onfulfilled => { 
+    client = onfulfilled;
+    db = client.db('WeServe');
+});
 
 module.exports = {
     firstMessage(request, response) {
@@ -14,15 +19,23 @@ module.exports = {
         return response.json(user);
     },
     async create(request, response) {
-        const user = await User.create(request.body);
-        return response.json(`${user.name} created!`);
+        const user = new User(request.body);
+        
+        try {
+            await user.save();
+            return response.send(`${user.name} created!\nData: ${user}`);
+        } catch(error) {
+            console.log(error);
+        }
+        //const user = await User.create(request.body);
+        //return response.json(`${user.name} created!\nData: ${user}`);
     },
     async update(request, response) {
         const user = await User.findByIdAndUpdate(request.params.id, request.body, { new: true });
         return response.json(`User ${user.name} updated!`);
     },
     async delete(request, response){
-        const user = await User.findByIdAndRemove(request.params.id);
+        const user = await User.findByIdAndDelete(request.params.id);
         return response.json('User deleted!');
     }
 }
