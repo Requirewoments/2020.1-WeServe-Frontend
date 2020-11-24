ebe8a0d0eb6d7033706bd806729315e22546581a
Maicon Mares <maiconmaresunb@gmail.com>
2020-11-24 07:39:03 -0300

    Instalando driver MongoDB e listando BD's.

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const {MongoClient} = require('mongodb');
 
 const app = express();
 
@@ -13,9 +14,31 @@ app.use(express.json());
 app.use(cors());
 app.use(routes);
 
-mongoose.connect('mongodb+srv://maiconmares:qnpjeposf4580@cluster0.pr5dc.mongodb.net/test', {
+const main = async () => {
+    const uri = 'mongodb%2Bsrv%3A%2F%2Fmaiconmares%3Aqnpjeposf4580%40cluster0.pr5dc.mongodb.net%2FWeServe%3FretryWrites%3Dtrue%26w%3Dmajority';
+    const client = new MongoClient(decodeURIComponent(uri), {useNewUrlParser: true, useUnifiedTopology: true});
+
+    try {
+        await client.connect();
+
+        await listDatabases(client);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const listDatabases = async client => {
+    let databasesList = await client.db().admin().listDatabases();
+
+    console.log('All is working! Databases:');
+    databasesList.databases.forEach(db => console.log(`- ${db.name}`));
+}
+
+main().catch(console.error);
+
+/*mongoose.connect('mongodb+srv://maiconmares:qnpjeposf4580@cluster0.pr5dc.mongodb.net/test', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+});*/
 
 app.listen(3003);
