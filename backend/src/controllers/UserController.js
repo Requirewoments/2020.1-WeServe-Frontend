710860fc257ba059fb467d2e11fd0031968a178d
Maicon Mares <maiconmaresunb@gmail.com>
2020-11-24 15:21:10 -0300

    Atualizando metodo para atualizar User.

--- backend/src/controllers/UserController.js
+++ backend/src/controllers/UserController.js
@@ -27,12 +27,15 @@ module.exports = {
         } catch(error) {
             console.log(error);
         }
-        //const user = await User.create(request.body);
-        //return response.json(`${user.name} created!\nData: ${user}`);
     },
     async update(request, response) {
-        const user = await User.findByIdAndUpdate(request.params.id, request.body, { new: true });
-        return response.json(`User ${user.name} updated!`);
+        try {
+            const user = await User.findByIdAndUpdate(request.params.id, request.body, { new: true });
+            await user.save();
+            return response.json(`User ${user.name} updated!`);
+        } catch (error) {
+            console.log(error);
+        }
     },
     async delete(request, response){
         const user = await User.findByIdAndDelete(request.params.id);
